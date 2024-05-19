5159d1048b8bc5609d0cb6c67e67355802d4f744
betaarnsburg <64945891+betaarnsburg@users.noreply.github.com>
2024-05-19 22:35:54 +0200

    Backend erstellen

---
+++ backendInsurance/src/main/java/com/example/backendinsurance/BackendInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInsuranceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendInsuranceApplication.class, args);
+    }
+
+}
